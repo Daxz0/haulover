6c6b5c92313506f244f611376b017391140f8d59
Daxz <59543649+Daxz0@users.noreply.github.com>
2026-05-23 20:28:18 -0700

    Watering Fix
    made it so that you cant abuse spam clicking
    made it so that the location youre looking at updates to get watered
    starter code for refilling

--- main/java/daxz/dev/haulover/Skills/Farming/FarmingTools/FarmingToolHandlers/WateringCanHandler.java
+++ main/java/daxz/dev/haulover/Skills/Farming/FarmingTools/FarmingToolHandlers/WateringCanHandler.java
@@ -1,21 +1,20 @@
 package daxz.dev.haulover.Skills.Farming.FarmingTools.FarmingToolHandlers;
 
-import com.destroystokyo.paper.event.player.PlayerReadyArrowEvent;
 import daxz.dev.haulover.Haulover;
-import io.papermc.paper.datacomponent.item.ChargedProjectiles;
 import io.papermc.paper.event.player.PlayerStopUsingItemEvent;
 import net.kyori.adventure.text.Component;
-import org.bukkit.Location;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Particle;
+import org.bukkit.*;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.Sound;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,30 +25,57 @@ public class WateringCanHandler implements Listener {
     private static final NamespacedKey wateringCanID = new NamespacedKey(Haulover.getInstance(), "haulover-watering_can");
 
     private static List<UUID> playersWatering = new ArrayList<>();
+    private static List<UUID> wateringRatelimit = new ArrayList<>();
 
     @EventHandler
     public void wateringCanUsage(PlayerInteractEvent event) {
         ItemStack item = event.getItem();
-        if (Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
-        Location loc = event.getClickedBlock().getLocation().toCenterLocation();
+        if (item == null) return;
+        if (!Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
         Player player = event.getPlayer();
+        if (wateringRatelimit.contains(player.getUniqueId())) return;
         playersWatering.remove(player.getUniqueId());
+        if (event.getAction() != Action.RIGHT_CLICK_AIR){
+            Location loc = event.getInteractionPoint();
+            Block clickedBlock = event.getClickedBlock();
+            if (clickedBlock != null && clickedBlock.getType() == Material.WATER) {
+                Sound drink = Sound.ENTITY_GENERIC_DRINK;
+                player.playSound(loc, drink, 1.0F, 1.0F);
+                return;
+            }
+        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+
+
+
+
+
         playersWatering.add(player.getUniqueId());
 
-
         new BukkitRunnable() {
+            int timeout = 0;
             @Override
             public void run() {
+                if (timeout >= 20) {
+                    cancel();
+                    return;
+                }
                 if (!playersWatering.contains(player.getUniqueId())) {
                     cancel();
                     return;
                 }
 
+                var targetBlock = player.getTargetBlockExact(3);
+                if (targetBlock == null) return;
+                Sound splash = Sound.ENTITY_GENERIC_SPLASH;
+                player.playSound(player.getLocation(), splash, 1.0F, 1.0F);
                 Particle.SPLASH.builder()
-                        .location(loc)
-                        .offset(0.3, 0.1, 0.3)
+                        .location(targetBlock.getLocation().toCenterLocation())
+                        .offset(0.2, 0.1, 0.2)
                         .count(25)
                         .spawn();
+
+                timeout++;
             }
         }.runTaskTimer(Haulover.getInstance(), 0L, 10L);
 
@@ -60,21 +86,33 @@ public class WateringCanHandler implements Listener {
     @EventHandler
     public void wateringCanCancelled(PlayerStopUsingItemEvent event) {
         ItemStack item = event.getItem();
-        if (Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
+        if (!Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
 
         Player player = event.getPlayer();
         playersWatering.remove(player.getUniqueId());
+        wateringRatelimit.add(player.getUniqueId());
+
+        player.setCooldown(item.getType(), 10);
+        Bukkit.getScheduler().runTaskLater(Haulover.getInstance(), () -> {
+            wateringRatelimit.remove(player.getUniqueId());
+
+        }, 10);
 
     }
 
     @EventHandler
     public void wateringCanUsed(PlayerItemConsumeEvent event) {
-        Player player = event.getPlayer();
         ItemStack item = event.getItem();
-        if (Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
-        
+        if (!Boolean.TRUE.equals(item.getItemMeta().getPersistentDataContainer().get(wateringCanID, PersistentDataType.BOOLEAN))) return;
 
+        Player player = event.getPlayer();
         playersWatering.remove(player.getUniqueId());
+        wateringRatelimit.add(player.getUniqueId());
+
+        Bukkit.getScheduler().runTaskLater(Haulover.getInstance(), () -> {
+            wateringRatelimit.remove(player.getUniqueId());
+
+        }, 20);
 
     }
 
--- main/java/daxz/dev/haulover/Skills/Farming/FarmingTools/WateringCans/BasicWateringCan.java
+++ main/java/daxz/dev/haulover/Skills/Farming/FarmingTools/WateringCans/BasicWateringCan.java
@@ -6,12 +6,18 @@ import io.papermc.paper.datacomponent.DataComponentType;
 import io.papermc.paper.datacomponent.DataComponentTypes;
 import io.papermc.paper.datacomponent.item.*;
 import io.papermc.paper.datacomponent.item.consumable.ItemUseAnimation;
+import net.kyori.adventure.key.Key;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 @SuppressWarnings("UnstableApiUsage")
 public class BasicWateringCan {
@@ -19,6 +25,7 @@ public class BasicWateringCan {
     public static final String ID = "basic_watering_can";
     public static final NamespacedKey hauloverItemID = new NamespacedKey(Haulover.getInstance(), "haulover_item");
     public static final NamespacedKey wateringCanID = new NamespacedKey(Haulover.getInstance(), "haulover-watering_can");
+    public static final NamespacedKey wateringCanCapacity = new NamespacedKey(Haulover.getInstance(), "haulover-watering_can-capacity");
 
     //flags
     public static final NamespacedKey stopConsume = new NamespacedKey(Haulover.getInstance(), "flag-no_consume");
@@ -29,18 +36,18 @@ public class BasicWateringCan {
         ItemStack item = ItemStack.of(material);
 
         float wateringCanSpeed = 3.0F;
-        int wateringCanCapacity = 100;
+        int wateringCanMaxCapacity = 100;
 
 
         item.setData(DataComponentTypes.CUSTOM_NAME, Component.text("Basic Watering Can", NamedTextColor.WHITE).decoration(TextDecoration.ITALIC, false));
-        item.setData(DataComponentTypes.CONSUMABLE, Consumable.consumable().animation(ItemUseAnimation.BOW).consumeSeconds(wateringCanSpeed).hasConsumeParticles(false));
-        item.setData(DataComponentTypes.ATTACK_RANGE, AttackRange.attackRange().maxReach(3).build());
+        item.setData(DataComponentTypes.CONSUMABLE, Consumable.consumable().animation(ItemUseAnimation.BOW).consumeSeconds(wateringCanSpeed).sound(Key.key("")).hasConsumeParticles(false));
+        item.setData(DataComponentTypes.USE_COOLDOWN, UseCooldown.useCooldown(1f).build());
 
         LoreTool.lore(item,
         "<dark_gray> A basic rusty watering can.",
               "<dark_gray><st>                                          </st>",
               "<gray>  Speed: <green>" + wateringCanSpeed + "s",
-              "<gray>  Capacity: <aqua>0<dark_aqua>/<dark_aqua>" + wateringCanCapacity,
+              "<gray>  Capacity: <aqua>0<dark_aqua>/<dark_aqua>" + wateringCanMaxCapacity,
               "<dark_gray><st>                                          </st>",
               "<white>\uD83C\uDF31 <bold>Farming Tool"
         );
@@ -49,6 +56,8 @@ public class BasicWateringCan {
         item.editPersistentDataContainer(pdc -> {
             pdc.set(hauloverItemID, PersistentDataType.STRING, ID);
             pdc.set(stopConsume, PersistentDataType.BOOLEAN, true);
+            pdc.set(wateringCanID, PersistentDataType.BOOLEAN, true);
+            pdc.set(wateringCanCapacity, PersistentDataType.FLOAT, 0f);
         });
         return item;
 
